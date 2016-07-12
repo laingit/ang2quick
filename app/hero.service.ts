0f6616358ec7e4cfaed5771132a153fa171625fb
Andrea Lai <alai@DSG.local>
2016-07-12 09:44:09 +0200

    6 take-it-slow example

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,4 +6,11 @@ export class HeroService {
   getHeroes() {
     return Promise.resolve(HEROES);
   }
+
+  getHeroesSlowly() {
+  return new Promise<Hero[]>(resolve =>
+    setTimeout( () => resolve(HEROES), 2000) // 2 seconds
+  );
+}
+
 }
